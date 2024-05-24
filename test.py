d5c9fc15ab559753789d5c8d83c06c8d0bbe3349
Yash Shukla <112846526+Yashshukla11@users.noreply.github.com>
2024-05-24 12:14:11 +0530

    updatee

--- test.py
+++ test.py
@@ -2,26 +2,31 @@ import os
 from rembg import remove
 from PIL import Image
 
-def process_images(input_folder, output_folder):
-
+def process_images(input_folders, output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    for filename in os.listdir(input_folder):
-        input_path = os.path.join(input_folder, filename)
-        output_path = os.path.join(output_folder, filename)
-
-        if not (filename.lower().endswith('.png') or filename.lower().endswith('.jpg') or filename.lower().endswith('.jpeg')):
+    for input_folder in input_folders:
+        if not os.path.exists(input_folder):
+            print(f"Warning: Input folder '{input_folder}' does not exist.")
             continue
 
-        with open(input_path, 'rb') as input_file:
-            input_data = input_file.read()
-            output_data = remove(input_data)
+        for filename in os.listdir(input_folder):
+            input_path = os.path.join(input_folder, filename)
+            output_path = os.path.join(output_folder, filename)
 
-            with open(output_path, 'wb') as output_file:
-                output_file.write(output_data)
+            if not (filename.lower().endswith('.png') or filename.lower().endswith('.jpg') or filename.lower().endswith('.jpeg')):
+                continue
 
-        print(f"Processed {filename}")
-input_folder = 'path/to/your/input/folder'
-output_folder = 'path/to/your/output/folder'
-process_images(input_folder, output_folder)
+            with open(input_path, 'rb') as input_file:
+                input_data = input_file.read()
+                output_data = remove(input_data)
+
+                with open(output_path, 'wb') as output_file:
+                    output_file.write(output_data)
+
+            print(f"Processed {filename} from {input_folder}")
+
+input_folders = ['/Users/yashshukla/Desktop/images', '/Users/yashshukla/Desktop/img']
+output_folder = '/Users/yashshukla/Desktop/image'
+process_images(input_folders, output_folder)
